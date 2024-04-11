7bb793a4b7a0224ee43cd63e47495d6dbc4d34e8
Crowdbotics <engineering@crowdbotics.com>
2024-04-11 12:44:41 +0000

    Initial commit

---
+++ backend/testdescsavechange_134877/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for testdescsavechange_134877 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "testdescsavechange_134877.settings"
+)
+
+application = get_wsgi_application()
